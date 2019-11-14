f09138b5bc629093918efa20a463a9e062f2032c
Juan Vieira <juanvieira@Juans-MacBook-Pro.local>
2019-11-14 19:02:55 +1100

    dates added to week buttons

--- src/components/habits/Habit.js
+++ src/components/habits/Habit.js
@@ -5,20 +5,28 @@ import HabitButton from './HabitButton';
 const Habit = props => {
   // properties
   const { habit } = props;
+  const dates = getLast5Days();
 
   // render
   return (
     <article>
       <h3>{habit}</h3>
       <div>
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
-        <HabitButton />
+        {dates.map(date => (
+          <HabitButton key={date.getMilliseconds()} date={date} />
+        ))}
       </div>
     </article>
   );
 };
 
 export default Habit;
+
+const getLast5Days = () => {
+  const dates = '01234'.split('').map(day => {
+    const tempDate = new Date();
+    tempDate.setDate(tempDate.getDate() - day);
+    return tempDate;
+  });
+  return dates;
+};
--- src/components/habits/HabitButton.js
+++ src/components/habits/HabitButton.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 
-const HabitButton = () => {
+const HabitButton = props => {
+  // properties
+  const { date } = props;
   const [complete, setComplete] = useState(false);
-  return <button onClick={() => setComplete(!complete)}>{complete ? 'X' : 'O'}</button>;
+
+  // render
+  return (
+    <span>
+      {date.getDate()} / {date.getMonth() + 1}
+      <button onClick={() => setComplete(!complete)}>{complete ? 'X' : 'O'}</button>
+    </span>
+  );
 };
 
 export default HabitButton;
