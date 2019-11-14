4bfb101c0f90bf646b7bd95cdfe171ced7764e7e
Juan Vieira <juanvieira@Juans-MacBook-Pro.local>
2019-11-14 15:54:38 +1100

    SSR without cache

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,15 +1,13 @@
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'isomorphic-unfetch';
+import Head from 'next/head';
 
 // higher order component to allow pages to access apollo Context. Wrapping _app.js causes issues
 export function withApollo(PageComponent) {
   const WithApollo = props => {
     // properties
-    const client = new ApolloClient({
-      uri: 'http://localhost:3000/api/graphql',
-      fetch, // we need to override the common apollo fetch to be able to use it on the client too
-    });
+    const client = initApolloClient();
 
     // render
     return (
@@ -19,5 +17,59 @@ export function withApollo(PageComponent) {
     );
   };
 
+  // setup getInitialProps to recover SSR in NextJS
+  WithApollo.getInitialProps = async ctx => {
+    const { AppTree } = ctx;
+    const apolloClient = (ctx.apolloClient = initApolloClient());
+
+    let pageProps = {};
+    if (PageComponent.getInitialProps) {
+      pageProps = await PageComponent.getInitialProps(ctx);
+    }
+
+    // if we are on the server
+    if (typeof window === 'undefined') {
+      if (ctx.res && ctx.res.finished) {
+        return pageProps;
+      }
+
+      // if we the response has not been finished
+      try {
+        const { getDataFromTree } = await import('@apollo/react-ssr');
+        // wait for the tree to load (with apollo context) and proceed
+        await getDataFromTree(
+          <AppTree
+            pageProps={{
+              ...pageProps,
+              apolloClient,
+            }}
+          />
+        );
+      } catch (error) {
+        console.log({ error });
+      }
+
+      // will clear tree
+      Head.rewind();
+    }
+
+    const apolloState = apolloClient.cache.extract();
+    return {
+      ...pageProps,
+      apolloState,
+    };
+  };
+
   return WithApollo;
 }
+
+const initApolloClient = () => {
+  const ssrMode = window === 'undefined';
+
+  const client = new ApolloClient({
+    ssrMode,
+    uri: 'http://localhost:3000/api/graphql',
+    fetch, // we need to override the common apollo fetch to be able to use it on the client too
+  });
+  return client;
+};
